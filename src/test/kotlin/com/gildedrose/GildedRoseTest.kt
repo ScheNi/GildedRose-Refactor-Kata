510793aed3185baa1d846bfb1562fc9e2b7b3b46
nicolasschelkens <nicolas.schelkens@appfoundry.be>
2020-01-19 11:01:14 +0100

    Add test cases for items with basic rules

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -13,6 +13,47 @@ class GildedRoseTest {
 
     }
 
+    @Test
+    fun `basic item decreases sellIn by one`() {
+        val item = Item("basic", 10, 4)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+
+        assertEquals(9, item.sellIn)
+    }
+
+
+    @Test
+    fun `basic item decreases quality by one when sellIn is greater than 0`() {
+        val item = Item("basic", 1, 4)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+
+        assertEquals(3, item.quality)
+    }
+
+    @Test
+    fun `basic item decreases quality twice as fast when sellIn is less than 1`() {
+        val item = Item("basic", 0, 4)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+
+        assertEquals(2, item.quality)
+    }
+
+    @Test
+    fun `basic item quality can not go negative`() {
+        val item = Item("basic", 0, 0)
+        val app = GildedRose(arrayOf(item))
+
+        app.updateQuality()
+
+        assertEquals(0, item.quality)
+    }
+
 
 }
 
