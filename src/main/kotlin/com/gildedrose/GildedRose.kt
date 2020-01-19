410dbd55b781a5f8e8367317701be1b79463d027
nicolasschelkens <nicolas.schelkens@appfoundry.be>
2020-01-19 11:00:36 +0100

    Add foreach & some comments

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,54 +3,80 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+        items.forEach { item ->
+            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality > 0) {
+                    // Item quality is greater than 0
+                    // Item is not aged brie & not backstage passes
+                    // -> Decrease quality by 1
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                // Item quality is less than 50
+                // -> Increase quality by 1
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.sellIn < 11) {
+                            // Item quality is less than 50
+                            // Item sellIn is less than 11
+                            // Item are Backstage passes
+                            // -> Increase quality by 1
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            // Item quality is less than 50
+                            // Item sellIn is less than 6
+                            // Item are Backstage passes
+                            // -> Increase quality by 1
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+
+            // Item is not Sulfuras
+            // -> decrease sellIn by 1
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.quality > 0) {
+                            // Item is over sellIn date,
+                            // Item is not aged brie, not backstage pass & not Sulfuras
+                            // Item quality is bigger than 0
+                            // -> Decrease quality by 1
+                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                                item.quality = item.quality - 1
                             }
                         }
+
+                        // Item is over sellIn date,
+                        // Item is backstage pass
+                        // -> Reset quality
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
 
                 } else {
 
-                    // Increase quality by one if it is aged rie && quality not >= 50
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    // Item quality is less than 50
+                    // Item is aged brie
+                    // -> increate quality by 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
