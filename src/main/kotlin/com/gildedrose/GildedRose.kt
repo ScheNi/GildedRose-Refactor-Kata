2a4ba0fa1a73baedd6b9cafecbd497e297261baf
nicolasschelkens <nicolas.schelkens@appfoundry.be>
2020-01-19 10:29:01 +0100

    Initial commit

---
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -0,0 +1,61 @@
+package com.gildedrose
+
+class GildedRose(var items: Array<Item>) {
+
+    fun updateQuality() {
+        for (i in items.indices) {
+            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].quality > 0) {
+                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        items[i].quality = items[i].quality - 1
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
+
+                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                items[i].sellIn = items[i].sellIn - 1
+            }
+
+            if (items[i].sellIn < 0) {
+                if (items[i].name != "Aged Brie") {
+                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].quality > 0) {
+                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                items[i].quality = items[i].quality - 1
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality
+                    }
+
+                } else {
+
+                    // Increase quality by one if it is aged rie && quality not >= 50
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                }
+            }
+        }
+    }
+
+}
+
